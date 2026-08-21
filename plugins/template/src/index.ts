79d17621d7a432ad484fb8a617bf5dca6dd0bbf6
Dreamhacker-Fear <lucaebraeuer@gmail.com>
2026-08-21 16:12:14 +0200

    Update logging in onLoad and onUnload methods

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -1,16 +1,11 @@
-import { metro } from "@vendetta/metro/common";
 import { logger } from "@vendetta";
 
 export default {
     onLoad() {
-        const GuildsBar = metro.findByName("GuildsBar");
-
-        logger.log(
-            GuildsBar
-                ? "Hidden Servers: GuildsBar FOUND"
-                : "Hidden Servers: GuildsBar NOT FOUND"
-        );
+        logger.log("Hidden Servers: TEST LOADED");
     },
 
-    onUnload() {},
+    onUnload() {
+        logger.log("Hidden Servers: TEST UNLOADED");
+    },
 };
