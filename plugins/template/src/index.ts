dface21001aefde3071dcb0dd214aefdf8b9227a
Dreamhacker-Fear <lucaebraeuer@gmail.com>
2026-08-21 16:04:09 +0200

    Simplify GuildsBar logging and cleanup code
    Refactored logging for GuildsBar presence and removed unpatch functionality.

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -1,35 +1,16 @@
 import { metro } from "@vendetta/metro/common";
-import { after } from "@vendetta/patcher";
 import { logger } from "@vendetta";
-
-let unpatch: (() => void) | null = null;
 
 export default {
     onLoad() {
         const GuildsBar = metro.findByName("GuildsBar");
 
-        if (!GuildsBar) {
-            logger.log("Hidden Servers: GuildsBar NOT FOUND");
-            return;
-        }
-
-        logger.log("Hidden Servers: GuildsBar FOUND");
-
-        unpatch = after("default", GuildsBar, (args, result) => {
-            logger.log(
-                "Hidden Servers: GuildsBar rendered"
-            );
-
-            logger.log(
-                `Hidden Servers: props = ${JSON.stringify(args?.[0] ?? {})}`
-            );
-
-            return result;
-        });
+        logger.log(
+            GuildsBar
+                ? "Hidden Servers: GuildsBar FOUND"
+                : "Hidden Servers: GuildsBar NOT FOUND"
+        );
     },
 
-    onUnload() {
-        unpatch?.();
-        unpatch = null;
-    },
+    onUnload() {},
 };
